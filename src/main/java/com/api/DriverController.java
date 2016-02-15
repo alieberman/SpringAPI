1790543ac321da9eef89bccba3f0fd0a5b7acad8
Aaron Lieberman <aaron@bigcompass.com>
2016-02-15 14:40:18 -0700

    Driver returns JSON

---
+++ src/main/java/com/api/DriverController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author aaronlieberman
+ */
+@RestController
+public class DriverController {
+    private static final String template = "%s";
+    private final int counter = 1;
+    
+    @RequestMapping("/driver")
+    public Driver driver(String name) {
+        return new Driver(String.format(template, "Aaron"));
+    }
+}
